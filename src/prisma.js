09b7c1e0072d0a5628a29008d48f91dc0d2dee02
dmitri_lipski <dzmitri.lipski@gmail.com>
2019-05-26 21:28:02 +0300

    Add some mutation methods

--- src/prisma.js
+++ src/prisma.js
@@ -2,5 +2,57 @@ import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'localhost:4466'
-})
+    endpoint: 'http://localhost:4466'
+});
+
+const createPostForUser = async (authorId, data) => {
+    const post = await prisma.mutation.createPost({
+        data: {
+            ...data,
+            author: {
+                connect: {
+                    id: authorId
+                }
+            }
+        }
+    }, '{ id }');
+    const user = await prisma.query.user({
+        where: {
+            id: authorId
+        }
+    }, '{ id name email posts { id title published } }');
+    return user
+};
+
+const updatePostForUser = async (postId, data) => {
+    const post = await prisma.mutation.updatePost({
+        where: {
+            id: postId
+        },
+        data
+    }, '{ author { id } }');
+
+    const user = await prisma.query.user({
+        where: {
+            id: post.author.id
+        }
+    }, '{ id name email posts { id title body published } }');
+    return user
+
+};
+
+// createPostForUser('cjw568lyc009f0772cf8cqjoz', {
+//     title: 'Great books to read',
+//     body: 'The War of Art',
+//     published: true
+// }).then((user) => {
+//     console.log(JSON.stringify(user, undefined, 2))
+// });
+
+updatePostForUser(
+    'cjw59j1ck00u40772yw9visdq',
+    { body: 'The War of Art!!!', published: false
+    }).then((user) => {
+    console.log(JSON.stringify(user, undefined, 2))
+});
+
